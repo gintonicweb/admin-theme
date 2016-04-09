6319bebba529c45c77af15cbee05e249d52d6485
Philippe Lafrance <phil.laf@gmail.com>
2016-04-09 15:16:08 -0400

    Selectize dropped to only work on select for now. TODO: ajax fetch for search

--- assets/js/crud-selectize.js
+++ assets/js/crud-selectize.js
@@ -7,54 +7,11 @@ define(function(require) {
     var $form = $input.closest('form');
     var searchField = $input.data('selectize');
     var url = $input.data('url');
-    var maxItems = $input.data('max-items');
-    var maxOptions = $input.data('max-options');
 
     $input.selectize({
         valueField: 'id',
-        labelField: searchField,
-        searchField: [searchField],
-        maxItems: maxItems,
-        maxOptions: maxOptions,
-        options: [],
-        create: false,
-        render: {
-            option: function (item, escape) {
-                return '<div>' + escape(item[searchField]) + '</div>';
-            }
-        },
-        load: function (query, callback) {
-            data = {};
-            data[searchField] = query;
-            $.ajax({
-                url: url,
-                dataType: 'json',
-                data: data,
-                error: function() {
-                    console.log('error fetching ' + searchField + ' from ' + url);
-                    callback();
-                },
-                success: function(res) {
-                    var results = $.map(res.data, function (name, id) {
-                        return {value: id, text: name};
-                    })
-                    callback(results);
-                }
-            });
-        },
+        maxItems: $input.data('max-items'),
+        maxOptions: $input.data('max-options'),
     });
-
-    $form.submit(function (){
-        var users = $input.val().split(",");
-        users.forEach(function(user){
-            $('<input />')
-                .attr('type', 'hidden')
-                .attr('name', $input.attr('name') + '[]')
-                .attr('value', user)
-                .appendTo($form);
-        });
-        $input.remove();
-    });
-
 });
 
