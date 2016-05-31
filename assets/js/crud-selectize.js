1df180e291eda2a1cc981e3972fa31698567c0b9
Philippe Lafrance <phil.laf@gmail.com>
2016-05-31 00:45:45 -0400

    assets update + selectize allows empty options via config param for now

--- assets/js/crud-selectize.js
+++ assets/js/crud-selectize.js
@@ -5,13 +5,13 @@ define(function(require) {
 
     var $input = $('[data-selectize]');
     var $form = $input.closest('form');
-    var searchField = $input.data('selectize');
     var url = $input.data('url');
 
     $input.selectize({
         valueField: 'id',
         maxItems: $input.data('max-items'),
         maxOptions: $input.data('max-options'),
+        allowEmptyOptions: $input.data('allow-empty-options'),
     });
 });
 
--- webroot/js/crud-selectize.js
+++ webroot/js/crud-selectize.js
@@ -1,1 +1,1 @@
-define(["require","jquery","selectize"],function(e){var t=e("jquery"),n=e("selectize"),r=t("[data-selectize]"),i=r.closest("form"),s=r.data("selectize"),o=r.data("url");r.selectize({valueField:"id",maxItems:r.data("max-items"),maxOptions:r.data("max-options")})});
+define(["require","jquery","selectize"],function(e){var t=e("jquery"),n=e("selectize"),r=t("[data-selectize]"),i=r.closest("form"),s=r.data("url");r.selectize({valueField:"id",maxItems:r.data("max-items"),maxOptions:r.data("max-options"),allowEmptyOptions:r.data("allow-empty-options")})});
